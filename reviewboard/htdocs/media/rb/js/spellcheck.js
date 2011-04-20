05216e1ae28ee6e46fa46aca3b0bc4c4d48234c6
dorothyk <kongmengyun@gmail.com>
2011-04-20 16:44:03 -0700

    merge spellcheck.js to differview.js

--- reviewboard/htdocs/media/rb/js/spellcheck.js
+++
@@ -1,8 +0,0 @@
-/*var gSpellCheck = new RB.SpellCheck();
-
-$('.spellerr').contextMenu('spellcheckMenu', {
-    bindings: {
-        'spellcheck_ignore': function(t) { gSpellCheck.ignoreOnce(t); },
-        'spellcheck_add': function(t) { gSpellCheck.addToDict(t); },
-    }
-});*/
